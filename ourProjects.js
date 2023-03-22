e3cbb90cd5ea239db99767ee7015dec5a95f238e
ArtonRamadani <60104374+ArtonRamadani@users.noreply.github.com>
2023-03-22 16:35:04 +0100

    cronjob.js

--- ourProjects.js
+++ ourProjects.js
@@ -1,120 +1,175 @@
-import { useState } from 'react';
-import { React, useEffect } from 'react'
-import { Link } from 'react-router-dom';
-import pageData from "../../DB/singlePageData.json";
-import "./ourProjects.scss"
+const nodemailer = require("nodemailer");
+let Costumer = require('../models/costumer');
+let Orders = require('../models/orders');
+const { QueryTypes, Sequelize } = require('sequelize');
 
-import { setLangData, setSelectedLang } from "../../redux/Functions/actions";
-import Translations from '../../components/translations/translations';
-import { connect } from 'react-redux';
+const db = require('../config/db');
 
+module.exports = async (userData, ordersForThisUser) => {
+    console.log('ordersForThisUser', ordersForThisUser)
 
-
-const OurProjects = (props) => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    getFiltersFromDB();
-  }, [window.location.reload])
-
-  const [shouldShowFilters, setShouldShowFilters] = useState("hideFilters")
-  const [filteredData, setFilteredData] = useState("")
-  const [filterOptions, setFilterOptions] = useState([])
-
-  function getFiltersFromDB() {
-    setFilterOptions([...new Set(pageData.map((el) => el.lloji_biznesit))])
-  }
-  // const map = new Map(Object.entries(filterOptions))
-  // console.log('mapRamadani', map)
-  function showFilter() {
-    setShouldShowFilters('showFilters');
-    if (shouldShowFilters == "showFilters") {
-      setShouldShowFilters("hideFilters");
-    }
-  }
-  function filterProjects(e) {
-    setFilteredData(pageData.filter((el) => el?.lloji_biznesit == e));
-  }
-  // console.log('filteredData', filteredData?.length)
-  return (
-    <div className="container ">
-      <div className=" allProjects">
-        <div className="tekstiProjekteveTengjashme">
-          <div className="row">
-            <h1 className='titulliProjekteve col-12'> {Translations(props?.langData, props?.selected_lang, 'projektetErealizuara')}</h1>
-            <div className="row ">
-
-              <p className='filter col-6'>
-                <button onClick={showFilter}>
-                  <i class="fas fa-light fa-sliders filter_icon" />  {Translations(props?.langData, props?.selected_lang, 'FiltroLabel')}
-                </button>
-              </p>
-              <p className='numrimiProjekteve col-6'>{Translations(props?.langData, props?.selected_lang, 'ShowingLabel')}: {filteredData?.length !== 0 ? filteredData?.length : pageData?.length} {Translations(props?.langData, props?.selected_lang, 'ProjekteLabel')}</p>
-            </div>
-            <div className={"container " + shouldShowFilters}>
-              <hr className='vijaNdarese col-12' />
-              <div className="d-flex flex-wrap">
-                {shouldShowFilters == "showFilters" ? filterOptions.map((el) =>
-                  <button className=' filter_buttons' onClick={() => { filterProjects(el) }}> 
-                  {Translations(props?.langData, props?.selected_lang, el)}
-                  {/* {el} */}
-                  </button>
-                )
-                  :
-                  ""
+    // let foundUser = await Costumer.findOne({ where: { email_sent: 0 } });
+    // console.log('foundUser123UUID', foundUser.uuid)
+    // if (foundUser !== null) {
+    // const ordersForThisUser = await await db.query(`select * from orders where order_id = ${foundUser.uuid}`);
+    if (ordersForThisUser.length !== 0) {
+        console.log('ordersForThisUser', ordersForThisUser)
+        try {
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'porosia.mtechnologie@gmail.com',
+                    pass: 'klsvzshsoizxglut'
                 }
-              </div>
-            </div>
+            });
+            let HelperOptions;
+            HelperOptions = {
+                from: '"" <porosia.mtechnologie@gmail.com>',
+                to: userData.email,
+                // cc: ccmail,
+                subject: 'Porosia juaj është pranuar! ',
+                html: `
+                        <!DOCTYPE html>
+                            <html>
+                            <head>
+                                <meta charset="utf-8" />
+                                <title>Your receipt</title>
+                                <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+                            </head>
+                            <body style="background-color: lightgray" bgcolor="lightgray">
+                                <!-- Main body table -->
+                                <table cellpadding="0" cellspacing="0" border="0" width="100%" style="font-family: sans-serif">
+                                    <tr>
+                                        <td height="10px">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">
+                                            <!-- Receipt table -->
+                                            <table border="0" cellpadding="5" cellspacing="0" style="width: 320px; background-color: #f2f4f5">
+                                                <tr>
+                                                    <td style="width: 100%; background-color: rgb(54, 35, 160); text-align: center">
+                                                        <img alt="m-technologie" width="50%" height="auto"
+                                                            src="http://www.m-technologie.com/static/media/companyLogo.41159162cd2083dec641.png" />
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center" style="color: rgb(54, 35, 160); font-size: x-large; font-weight: bolder">
+                                                        Kërkesa juaj për ofertë është pranuar.
+                                                    </td>
+                                                </tr>
+                                                <!-- Receipt items -->
+                                                <tr>
+                                                    <td>
+                                                        <table>
+                                                            <tr>
+                                                                <td colspan="2" style="
+                                                                    border-top-width: 1px;
+                                                                    border-top-color: #e0e1e2;
+                                                                    border-top-style: dashed;
+                                                                    line-height: 0px;
+                                                                    font-size: 0;
+                                                                    border-collapse: collapse;
+                                                                    padding: 0;
+                                                                    "height="1">
+                                                                    &nbsp;
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td colspan="2" height="5px"></td>
+                                                            </tr>
+                                                           
+                                                            ${ordersForThisUser[0]?.map((el) => {
+                                                                return (
+                                                                         `
+                                                                         <tr>
+                                                                            <td width="101%" align="center">Shuco MV8, ${el?.selected_color}, ${el?.defined_height}x${el?.defined_width}cm, ${el?.selected_blinds}</td>
+                                                                        </tr>
+                                                                         `
+                                                                        )
+                                                            })}
+                                                          
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                                
+                                                <!-- Address -->
+                                                <tr>
+                                                    <td align="center">
+                                                        <span style="font-weight: bold">Emri: ${userData?.name_surname}</span><br />
+                                                        Adresa: ${userData?.address}<br />
+                                                        Qyteti: ${userData?.city} <br />
+                                                        Numri kontaktues: ${userData?.phone_number}
+                                                    </td>
+                                                </tr>
+                                                <!-- Transaction table -->
+                                                <tr>
+                                                    <td align="center">
+                                                        <table style="font-size: smaller; color: #85898c; width: 100%">
+                                                            <tr>
+                                                                <td colspan="2" style="
+                                                                    border-top-width: 1px;
+                                                                    border-top-color: #e0e1e2;
+                                                                    border-top-style: dashed;
+                                                                    line-height: 0px;
+                                                                    font-size: 0;
+                                                                    border-collapse: collapse;
+                                                                    padding: 0;
+                                                                " height="1">
+                                                                    &nbsp;
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td colspan="2" height="5px"></td>
+                                                            </tr>
+                                                           
+                                                        </table>
+                                                        <tr>
+                                                               
+                                                            
+                                                        <td style="width: 100%; background-color: rgb(54, 35, 160); text-align: center">
+                                                        <img alt="m-technologie" width="50%" height="auto"
+                                                            src="http://www.m-technologie.com/static/media/companyLogo.41159162cd2083dec641.png" />
+                                                    </td>
 
-          </div>
-        </div>
-        <div className="row mt-4">
-          {filteredData !== "" ?
-            filteredData?.map((el) => {
-              return (
-                <Link data-aos="fade-right" data-aos-duration="1000" className=" col-md-6 col-12 singleCard" to={el?.pageUniqueURL}>
-                  <div className='' style={{ backgroundImage: `url(${el?.bg_img})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "415px" }} />
-                  <div className="image_info">
-                    <h5 className="cardTitle mb-5">{el?.obijektiName}</h5>
-                    <div className="cardText mb-3 ">
-                      <i className="fas fa-map-marker-alt  " aria-hidden="true" /> &nbsp;{el?.location}
-                    </div>
-                  </div>
-                </Link>
-              )
-            })
-            :
-            pageData?.map((el) => {
-              return (
-                <Link data-aos="fade-right" data-aos-duration="1000" className=" col-md-6 col-12 singleCard" to={el?.pageUniqueURL}>
-                  <div className='' style={{ backgroundImage: `url(${el?.bg_img})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "415px" }} />
-                  <div className="image_info">
-                    <h5 className="cardTitle mb-5">{el?.obijektiName}</h5>
-                    <div className="cardText mb-3 ">
-                      <i className="fas fa-map-marker-alt  " aria-hidden="true" /> &nbsp;{el?.location}
-                    </div>
-                  </div>
-                </Link>
-              )
-            })}
-        </div>
-        <hr className='vijaNdarese col-12 mb-4 mt-2' />
-      </div>
 
-    </div>
-  )
+
+
+
+
+
+
+                                                        </tr>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                   
+                                </table>
+                            </body>
+
+                            </html>
+                `, // html body,
+            };
+            return transporter.sendMail(HelperOptions, async (error, info) => {
+                console.log("HelperOptionsRamadani", HelperOptions)
+                if (error) {
+                    let changeEmailStatus = await db.query(`UPDATE costumers SET email_sent = 0 where uuid = ${userData?.uuid}`)
+                    console.log("Email not sent", error);
+                    // console.log(`Error during cronjob: ${error}; Time: ${new Date()};`)
+                } else {
+                    
+                    console.log('userData', userData)
+                    console.log('ordersForThisUser', ordersForThisUser)
+                    // console.log('uuid', uuid)
+                }
+            });
+        } catch (err) {
+            // console.log('userData', userData)
+
+            console.log(`Error during cronjob2: ${err}; Time: ${new Date()}`);
+            return "Error";
+        }
+    }
 }
-const mapStateToProps = state => {
-  return {
-      selected_lang: state.data.selected_lang,
-      langData: state.data.langData,
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-      setLangData: () => dispatch(setLangData()),
-      setSelectedLang: (lang) => dispatch(setSelectedLang(lang))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(OurProjects);
+// }
